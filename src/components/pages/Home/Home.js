a76374d5b9fab23929a9065232f49da81903b8bf
Grzegorz Staszczak <grzestamail@gmail.com>
2023-03-20 16:43:38 +0100

    Add link to add post

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import RenderPosts from '../../features/RenderPosts/RenderPosts';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <main>
       <div className='main-title d-flex flex-row mb-5'>
         <h1 className='m-0 fs-3 text-bold'>All Posts</h1>
-        <Button variant='outline-success' className='ms-auto'>
-          Edit
-        </Button>
+        <Link to={`post/add`} className='ms-auto'>
+          <Button variant='outline-success'>Add post</Button>
+        </Link>
       </div>
       <RenderPosts />
     </main>
